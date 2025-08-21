dca3a9ea78269bcdaabcda3f1dc7583e4c1c66e3
gbuckle10 <74106972+gbuckle10@users.noreply.github.com>
2025-08-21 16:10:07 +0200

    Dorado download now works for windows and linux

--- script.sh
+++ script.sh
@@ -9,31 +9,62 @@
 install_dorado() {
   local VERSION=$1
   echo "--- Downloading and setting up Dorado ---"
+
+  echo "-- Detecting OS --"
+  case "$(uname -s)" in
+    Linux*)
+      os_type="linux-x64"
+      archive_filename="dorado.tar.gz"
+      download_url="https://cdn.oxfordnanoportal.com/software/analysis/dorado-$VERSION-${os_type}.tar.gz"
+      ;;
+    CYGWIN*|MINGW*|MSYS*)
+      os_type="win64"
+      archive_filename="dorado.zip"
+      download_url="https://cdn.oxfordnanoportal.com/software/analysis/dorado-$VERSION-${os_type}.zip"
+      ;;
+    *)
+      echo "I don't have a clue what operating system you're trying to use..."
+      return 1
+      ;;
+  esac
+  echo "The OS has been found, it's $os_type"
+
   echo "--- Checking for existing Dorado installation ---"
-  if [ -d "tools/dorado_v$VERSION" ]; then
+  expected_folder=tools/dorado-$VERSION-${os_type}
+  if [ -d "tools/dorado-$VERSION-${os_type}" ]; then
     echo -e "Dorado folder already exists"
+    LOCAL_DORADO_BIN="tools/dorado-$VERSION-${os_type}"
+    export PATH="$LOCAL_DORADO_BIN:$PATH"
     return 0
   fi
 
-  echo "Dorado not found, so I'll install it now."
+  echo "Dorado folder ${expected_folder} not found, so I'll install it now."
 
   echo "Creating installation directory in tools"
   mkdir -p "tools"
 
-  echo "Downloading dorado from https://cdn.oxfordnanoportal.com/software/analysis/dorado-$VERSION-win64.zip"
-  curl -o tools/dorado.zip  "https://cdn.oxfordnanoportal.com/software/analysis/dorado-$VERSION-win64.zip"
+  # Include a check here for whether the archive_filename is there to save more time.
+  echo "Downloading dorado version $VERSION for ${os_type} from ${download_url}"
+  curl -o tools/${archive_filename}  $download_url
 
-  echo "Downloading dorado version 0.9.6 from https://cdn.oxfordnanoportal.com/software/analysis/dorado-0.9.6-win64.zip"
   echo "Download complete. Unzipping and setting up."
 
-  unzip -q tools/dorado.zip -d tools
-  mv "tools/dorado-$VERSION-win64"* "tools/dorado_v$VERSION"
-  rm tools/dorado.zip
+  case ${os_type} in
+    "linux-x64")
+      tar -xzvf "tools/${archive_filename}" -C "tools/"
+      ;;
+    "win64")
+      unzip -q "tools/${archive_filename}" -d "tools/"
+      ;;
+  esac
+
+  mv "tools/dorado-$VERSION-${os_type}"* "tools/dorado_v$VERSION-${os_type}"
+  rm tools/${archive_filename}
 
   echo "-------- Dorado was successfully installed ---------------"
 
   echo "-------- Setting Dorado exe to path --------------"
-  LOCAL_DORADO_BIN="tools/dorado_v$VERSION/bin"
+  LOCAL_DORADO_BIN="tools/dorado-$VERSION-${os_type}"
   export PATH="$LOCAL_DORADO_BIN:$PATH"
 
 
@@ -58,7 +89,7 @@ download_fast5_data() {
   #aws s3 ls s3://ont-open-data/rrms_2022.07/flowcells/Benchmarking_ASmethylation_COLO829_1-5/COLO829_1/20211102_1709_X1_FAR52193_a64b5c94/fast5_pass/ --no-sign-request | head -n 20 | awk '{print $4}' | xargs -I {} aws s3 cp s3://ont-open-data/rrms_2022.07/flowcells/Benchmarking_ASmethylation_COLO829_1-5/COLO829_1/20211102_1709_X1_FAR52193_a64b5c94/fast5_pass/{} data/fast5_input/ --no-sign-request
 
   aws s3 ls s3://ont-open-data/rrms_2022.07/flowcells/Benchmarking_ASmethylation_COLO829_1-5/COLO829_1/20211102_1709_X1_FAR52193_a64b5c94/fast5_pass/ --no-sign-request \
-  | head -n 20 \
+  | head -n 3 \
   | awk '{print $4}' \
   | while read -r filename; do
       aws s3 cp s3://ont-open-data/rrms_2022.07/flowcells/Benchmarking_ASmethylation_COLO829_1-5/COLO829_1/20211102_1709_X1_FAR52193_a64b5c94/fast5_pass/"$filename" data/fast5_input/"$filename" --no-sign-request
@@ -68,19 +99,22 @@ download_fast5_data() {
 convert_fast5_to_pod5() {
   echo "Converting fast5 to pod5"
 
-  mkdir data/pod5_output/all_reads.pod5
+  mkdir -p data/pod5_output/all_reads.pod5
 
   pod5 convert fast5 data/fast5_input --output data/pod5_output/all_reads.pod5
 }
 
 basecalling_pod5() {
   # This line doesn't work on the old fast5 data I found online...it will work for new ones but the data I downloaded is too old for this version of dorado
-  dorado basecaller hac data/pod5_output/all_reads.pod5/output.pod5 > data/basecalled_output/calls.bam
+  dorado basecaller hac data/pod5_output/all_reads.pod5 > data/basecalled_output/calls.bam
 }
 
 run_script(){
-  DORADO_VERSION="1.1.1"
+  DORADO_VERSION="0.9.6"
   install_dorado "$DORADO_VERSION"
+  download_fast5_data
+  convert_fast5_to_pod5
+  basecalling_pod5
 }
 
 run_script
