fdc34e821481f69e5f1eb171de6c6c7e611b5d8d
gbuckle10 <74106972+gbuckle10@users.noreply.github.com>
2025-09-03 14:24:41 +0200

    All requirements and configurations for the setup script are in yml and yaml files. Pipeline controller can now run the 00_setup.sh script inside the conda environment.

--- pipeline_controller.py
+++ pipeline_controller.py
@@ -9,8 +9,8 @@ def load_config(config_file="config.yaml"):
     with open(config_file, 'r') as f:
         return yaml.safe_load(f)
 
-def run_in_conda(config, command):
-    """ Runs a command inside the conda environment """
+def run_and_capture(config, command):
+    """ Runs a command inside the conda environment and stores the output as a variable"""
     conda_env = config['conda_env_name']
 
     full_command = ["conda", "run", "-n", conda_env] + command
@@ -19,6 +19,9 @@ def run_in_conda(config, command):
 
     try:
         result = subprocess.run(full_command, check=True, capture_output=True, text=True)
+        if result.stderr:
+            print(result.stderr.strip())
+        return result.stdout.strip()
     except subprocess.CalledProcessError as e:
         print(f"--- ERROR IN COMMAND ---")
         print(f"Exit code: {e.returncode}")
@@ -26,20 +29,51 @@ def run_in_conda(config, command):
         print(f"STDERR:\n{e.stderr}")
         raise
 
+    print(result)
+
+def run_and_stream(config, command):
+    """ Runs a command inside the conda environment and streams the output """
+    conda_env = config['conda_env_name']
+
+    full_command = ["conda", "run", "-n", conda_env] + command
+
+    print(f"--- Running : {' '.join(full_command)} --- ")
+
+    try:
+        result = subprocess.run(full_command, check=True, text=True)
+    except subprocess.CalledProcessError as e:
+        print(f"--- ERROR IN COMMAND ---")
+        print(f"Exit code: {e.returncode}")
+        print(f"STDOUT:\n{e.stdout}")
+        print(f"STDERR:\n{e.stderr}")
+        raise
+
+    print(result)
+
 def run_setup(config):
     """ Executes the 00_setup.sh script """
     print(">>> Starting step 0: Setup")
     script_path = "scripts/00_setup.sh"
+
+    tasks = config.get('run_setup_tasks', {})
+
+    download_fast5 = str(tasks.get('download_fast5_data', False)).lower()
+    convert_fast5_to_pod5 = str(tasks.get('convert_fast5_to_pod5', False)).lower()
+
     command = [
         "bash", script_path,
-        config['reference_genome_url'],
-        config['reference_genome_dir'],
-        config['reference_fasta'],
-        config['reference_index'],
-        config['dorado_version']
+        config['dorado_version'],
+        config['fast5_download_url'],
+        config['fast5_input_dir'],
+        config['num_fast5_files'],
+        config['pod5_dir'],
+        download_fast5,
+        convert_fast5_to_pod5
     ]
 
-    run_in_conda(config, command)
+    dorado_path = run_and_capture(config, command)
+    #run_and_stream(config, command)
+    print(dorado_path)
 
 def main():
     """ Main entry point for the pipeline controller """
--- scripts/00_setup.sh
+++ scripts/00_setup.sh
@@ -1,71 +1,133 @@
 #!/bin/bash
 
+set -e
+
+DORADO_VERSION=$1
+FAST5_DOWNLOAD_URL=$2
+FAST5_DESTINATION_DIR=$3
+NUM_FAST5_FILES=$4
+POD5_DIR=$5
+SHOULD_DOWNLOAD_FAST5_FILES=$6
+SHOULD_CONVERT_FAST5_TO_POD5=$7
+
+make_directories() {
+  mkdir -p data
+  mkdir -p logs
+  mkdir -p models
+  mkdir -p reference_genomes
+  mkdir -p scripts
+  mkdir -p tools
+}
+
 
 install_dorado() {
-  local VERSION=$1
-  echo "--- Downloading and setting up Dorado ---"
+  echo "--- Downloading and setting up Dorado ---" >&2
 
-  echo "-- Detecting OS --"
+  echo "-- Detecting OS --" >&2
   case "$(uname -s)" in
     Linux*)
       os_type="linux-x64"
       archive_filename="dorado.tar.gz"
-      download_url="https://cdn.oxfordnanoportal.com/software/analysis/dorado-$VERSION-${os_type}.tar.gz"
+      download_url="https://cdn.oxfordnanoportal.com/software/analysis/dorado-${DORADO_VERSION}-${os_type}.tar.gz"
       ;;
     CYGWIN*|MINGW*|MSYS*)
       os_type="win64"
       archive_filename="dorado.zip"
-      download_url="https://cdn.oxfordnanoportal.com/software/analysis/dorado-$VERSION-${os_type}.zip"
+      download_url="https://cdn.oxfordnanoportal.com/software/analysis/dorado-${DORADO_VERSION}-${os_type}.zip"
       ;;
     *)
-      echo "I don't have a clue what operating system you're trying to use..."
+      echo "I don't have a clue what operating system you're trying to use..." >&2
       return 1
       ;;
   esac
-  echo "The OS has been found, it's $os_type"
+  echo "The OS has been found, it's $os_type" >&2
 
-  dorado_folder=tools/dorado-${VERSION}-${os_type}
-  echo "--- Checking for existing Dorado installation in ${dorado_folder}---"
-  if [ -d "${dorado_folder}" ]; then
-    echo "Dorado folder already exists"
-    LOCAL_DORADO_BIN="${dorado_folder}/bin"
-    export PATH="$LOCAL_DORADO_BIN:$PATH"
-    echo "The local dorado binary is in ${LOCAL_DORADO_BIN}"
-    return 0
+  DORADO_DIR=tools/dorado-${DORADO_VERSION}-${os_type}
+  echo "--- Checking for existing Dorado installation in ${DORADO_DIR}---"
+  if [ -d "${DORADO_DIR}" ]; then
+    echo "Dorado folder already exists" >&2
+
+  else 
+    echo "Dorado folder ${DORADO_DIR} not found, so I'll install it now." >&2
+
+    # Include a check here for whether the archive_filename is there to save more time.
+    echo "Downloading dorado version ${DORADO_VERSION} for ${os_type} from ${download_url}" >&2
+    curl -o tools/${archive_filename}  "$download_url"
+  
+    echo "Download complete. Unzipping and setting up." >&2
+  
+    case ${os_type} in
+      "linux-x64")
+        tar -xzvf "tools/${archive_filename}" -C "tools/"
+        ;;
+      "win64")
+        unzip -q "tools/${archive_filename}" -d "tools/"
+        ;;
+    esac
+  
+    folder_to_remove=tools/${archive_filename}
+    echo "Removing ${folder_to_remove}" >&2
+  
+    rm "${folder_to_remove}"
+    
+    
+  fi
+  
+  #LOCAL_DORADO_BIN="${DORADO_DIR}/bin"
+  #export PATH="$LOCAL_DORADO_BIN:$PATH"
+  #echo "The local dorado binary is in ${LOCAL_DORADO_BIN}"
+  
+  
+  DORADO_BIN_PATH=$(find "$(pwd)/${DORADO_DIR}" -type f -name dorado -executable)
+
+  if [ -z "${DORADO_BIN_PATH}" ]; then
+    echo "ERROR: Could not find the dorado executable after setup" >&2
+    exit 1
   fi
 
-  echo "Dorado folder ${dorado_folder} not found, so I'll install it now."
+  echo "$DORADO_BIN_PATH"
 
-  echo "Creating installation directory in tools"
-  mkdir -p "tools"
+  echo "-------- Dorado was successfully installed ---------------" >&2
+}
 
-  # Include a check here for whether the archive_filename is there to save more time.
-  echo "Downloading dorado version $VERSION for ${os_type} from ${download_url}"
-  curl -o tools/${archive_filename}  "$download_url"
+download_fast5_data() {
+  #alias mkdir='/usr/bin/mkdir'
 
-  echo "Download complete. Unzipping and setting up."
 
-  case ${os_type} in
-    "linux-x64")
-      tar -xzvf "tools/${archive_filename}" -C "tools/"
-      ;;
-    "win64")
-      unzip -q "tools/${archive_filename}" -d "tools/"
-      ;;
-  esac
+  echo "----Starting Data Download Script----"
 
-  folder_to_remove=tools/${archive_filename}
-  echo "Removing ${folder_to_remove}"
 
-  rm "${folder_to_remove}"
+  echo "--- Making fast5 input folder at ${FAST5_DESTINATION_DIR}---"
+  mkdir -p "${FAST5_DESTINATION_DIR}"
 
-  echo "-------- Dorado was successfully installed ---------------"
+  echo "--- Downloading ${NUM_FAST5_FILES} fast5 files into '${FAST5_DESTINATION_DIR}' ---"
+  #aws s3 ls s3://ont-open-data/rrms_2022.07/flowcells/Benchmarking_ASmethylation_COLO829_1-5/COLO829_1/20211102_1709_X1_FAR52193_a64b5c94/fast5_pass/ --no-sign-request | head -n 5 | awk '{print $4}' | xargs -I {} aws s3 cp s3://ont-open-data/rrms_2022.07/flowcells/Benchmarking_ASmethylation_COLO829_1-5/COLO829_1/20211102_1709_X1_FAR52193_a64b5c94/fast5_pass/{} data/fast5_input/ --no-sign-request
+  echo "--- Downloading from '${FAST5_DOWNLOAD_URL}'"
 
-  echo "-------- Setting Dorado exe to path --------------"
-  LOCAL_DORADO_BIN="${dorado_folder}/bin"
-  export PATH="$LOCAL_DORADO_BIN:$PATH"
-  echo "The local dorado binary is in ${LOCAL_DORADO_BIN}"
+  aws s3 ls "${FAST5_DOWNLOAD_URL}" --no-sign-request \
+  | head -n "${NUM_FAST5_FILES}" \
+  | awk '{print $4}' \
+  | while read -r filename; do
+      aws s3 cp "${FAST5_DOWNLOAD_URL}${filename}" "${FAST5_DESTINATION_DIR}""$filename" --no-sign-request || true
+  done
 
 }
 
-install_dorado "0.9.6"
+convert_fast5_to_pod5() {
+  echo "--- Converting fast5 to pod5 ---"
+
+  mkdir -p "${POD5_DIR}"
+
+  pod5 convert fast5 "${FAST5_DESTINATION_DIR}" --output "${POD5_DIR}/all_reads.pod5" --force-overwrite
+}
+
+
+make_directories
+install_dorado
+if [[ "$SHOULD_DOWNLOAD_FAST5_FILES" == "true" ]]; then
+  download_fast5_data
+fi
+if [[ "$SHOULD_CONVERT_FAST5_TO_POD5" == "true" ]]; then
+  convert_fast5_to_pod5
+fi
+
