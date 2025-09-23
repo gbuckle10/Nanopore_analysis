f0e5fd92097daa98672d852f5d47effa34ec2ac1
gbuckle10 <74106972+gbuckle10@users.noreply.github.com>
2025-09-23 17:11:50 +0200

    Basecalling script logged properly.

--- scripts/01_basecalling.sh
+++ scripts/01_basecalling.sh
@@ -1,65 +1,62 @@
 #!/bin/bash
 
+# Exit on error
 set -e
 
-CONFIG_FILE=$1
-
+# Check to make sure that the runtime_config is there.
 if [ ! -f "scripts/runtime_config.sh" ]; then
-  echo "ERROR: runtime_config.sh not found. Please run 'setup' step first..."
+  log_error "ERROR: runtime_config.sh not found. Please run 'setup' step first..."
   exit 1
 fi
+
+# Source the logging utility and runtime_config
+# Assumes that logging.sh is in the utils folder of the current folder.
+source "$(dirname "$0")/utils/logging.sh"
 source "scripts/runtime_config.sh"
 
 DORADO_MODEL_NAME=$(yq e '.parameters.basecalling.base_model_name' "${CONFIG_FILE}")
 MODEL_SPEED=$(yq e '.parameters.basecalling.model_speed' "${CONFIG_FILE}")
 BASECALLING_MODIFICATIONS=$(yq e '.parameters.basecalling.basecalling_modifications' "${CONFIG_FILE}")
 BATCHSIZE=$(yq e '.parameters.analysis.batch_size' "${CONFIG_FILE}")
+POD5_DIR=$(yq e '.paths.pod5_dir' "${CONFIG_FILE}")
+POD5_NAME=$(yq e '.paths.pod5_name' "${CONFIG_FILE}")
+BASECALLING_DIR=$(yq e '.paths.basecalled_output_dir' "${CONFIG_FILE}")
+BASECALLED_FILE=$(yq e '.paths.unaligned_bam_name' "${CONFIG_FILE}")
 MODEL_DIR="models/"
 
 # --- Main script ---
-echo "INFO: Using Dorado executable at ${DORADO_EXECUTABLE}" >&2
-
-
-check_device_for_dorado() {
-  if command -v nvidia-smi &> /dev/null; then
-    echo "NVIDIA GPU found, so we'll use that for basecalling"
-
-    # Tell dorado to tun with --device flag cuda:0
-    export DORADO_DEVICE_FLAG="--device cuda:0"
-    # Default batchsize is 128
-    export DORADO_BATCHSIZE_FLAG="--batchsize 128"
-
-  else
-    echo "No NVIDIA GPU found, so we'll run with the CPU only"
-
-    # Tell dorado to run with --device flag cpu
-    export DORADO_DEVICE_FLAG="--device cpu"
-    # Default batchsize is 128
-    export DORADO_BATCHSIZE_FLAG=""
-  fi
-
-  echo "Dorado device flags set: ${DORADO_DEVICE_FLAG} ${DORADO_BATCHSIZE_FLAG}"
-}
+log_info "Using Dorado executable at ${DORADO_EXECUTABLE}" >&2
 
 download_dorado_model() {
   #dna_r9.4.1_e8_fast@v3.4_5mCG_5hmCG@v0
-  echo "Downloading Dorado model"
+  log_info "Downloading Dorado model ${DORADO_MODEL_NAME} into ${MODEL_DIR}"
   "${DORADO_EXECUTABLE}" download --model dna_r9.4.1_e8_fast@v3.4 --models-directory "${MODEL_DIR}"
-  "${DORADO_EXECUTABLE}" download --model dna_r9.4.1_e8_fast@v3.4_5mCG_5hmCG@v0 --models-directory "${MODEL_DIR}"
-
+  #"${DORADO_EXECUTABLE}" download --model dna_r9.4.1_e8_fast@v3.4_5mCG_5hmCG@v0 --models-directory "${MODEL_DIR}"
 }
 
 basecalling_pod5() {
-  echo "Adding dorado to the path"
-  local POD5_INPUT="data/pod5_output/all_pod.pod5"
-  local BASECALLED_OUTPUT="data/basecalled_output/calls.bam"
+  local POD5_INPUT="${POD5_DIR}${POD5_NAME}"
+  local BASECALLED_OUTPUT="${BASECALLING_DIR}${BASECALLED_FILE}"
 
-  # ---- You need to add the download_dorado_model here
   #  download_dorado_model
   # ---- You could also make it so that the alignment and basecalling is done at once. That should be the next thing to test.
 
+  local dorado_cmd=(
+    "${DORADO_EXECUTABLE}"
+    basecaller
+    "${MODEL_SPEED},${BASECALLING_MODIFICATIONS}"
+    "${POD5_INPUT}"
+    --batchsize "${BATCHSIZE}"
+  )
+
+  log_info "Executing command: ${dorado_cmd[*]} > ${BASECALLED_OUTPUT}"
+
+  "${dorado_cmd[@]}" > "${BASECALLED_OUTPUT}"
+
+
+
   #"${DORADO_EXECUTABLE}" basecaller "${MODEL_SPEED},${BASECALLING_MODIFICATIONS}" --modified-bases "${BASECALLING_MODIFICATIONS}" --batchsize "${BATCHSIZE}" data/pod5_output/all_reads.pod5 > data/basecalled_output/newcalls.bam
-  "${DORADO_EXECUTABLE}" basecaller fast,5mCG_5hmCG data/pod5_output/all_pod.pod5 --batchsize 64  > data/basecalled_output/newcalls2.bam
+  #"${DORADO_EXECUTABLE}" basecaller "${MODEL_SPEED},${BASECALLING_MODIFICATIONS}" "${POD5_INPUT}" --batchsize "${BATCHSIZE}"  > data/basecalled_output/newcalls2.bam
   #"${DORADO_EXECUTABLE}" basecaller fast --mpd5mCG_5hmCG --batchsize 64 data/pod5_output/all_reads.pod5 > data/basecalled_output/newcalls.bam
 
   # Output bam file can be checked out with samtools - if we want to see whether there's any methylation in the bam file use
