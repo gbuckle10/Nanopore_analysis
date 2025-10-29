14d8eb8065c30a376cb023176124b004ef0dfcf2
gbuckle10 <74106972+gbuckle10@users.noreply.github.com>
2025-10-29 14:36:28 +0100

    io_parser now just adds io_params

--- src/utils/cli_utils.py
+++ src/utils/cli_utils.py
@@ -1,29 +1,40 @@
 import argparse
 from pathlib import Path
 
+from src.config.models import AppSettings
 
-def add_input_file_argument(parser, help_text="Path to the input file/directory"):
+
+def add_input_file_argument(parser, default, help_text):
     parser.add_argument(
         "--input-file",
         type=Path,
-        default=None,
+        default=default,
         help=f"{help_text} (Defaults to value in config file)"
     )
 
 
-def add_output_dir_argument(parser, help_text="Path to the output file/directory"):
+def add_output_dir_argument(parser, default, help_text):
     parser.add_argument(
         "-o", "--output-dir",
         type=Path,
-        default=None,
+        default=default,
         help=f"{help_text} (Defaults to value in config file)"
     )
 
-def create_io_parser():
+
+def add_io_arguments(
+        parser: argparse.ArgumentParser,
+        config: AppSettings,
+        default_input: Path = None,
+        default_output: Path = None,
+        input_file_help: str = "Path to the input file/directory",
+        output_dir_help: str = "Path to the output file/directory"
+):
     """
-    Creates and returns a parent parser with standard I/O arguments.
+    Adds standard I/O arguments to a given parser with customisable help messages and defaults
     """
-    io_parser = argparse.ArgumentParser(add_help=False)
-    add_input_file_argument(io_parser)
-    add_output_dir_argument(io_parser)
-    return io_parser
+    if default_output is None:
+        default_output = config.paths.root
+
+    add_input_file_argument(parser, default_input, input_file_help)
+    add_output_dir_argument(parser, default_output, output_dir_help)
