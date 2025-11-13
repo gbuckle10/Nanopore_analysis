3f0c7866a5e5d09b8cf55b45fd484fa3cfe55b0d
gbuckle10 <georgebuckle10@gmail.com>
2025-11-13 13:25:52 +0100

    Handler to deal with multi-line output.

--- src/utils/process_utils.py
+++ src/utils/process_utils.py
@@ -13,6 +13,109 @@ logger = logging.getLogger('pipeline')
 FULL_ANSI_REGEX = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 LAYOUT_CONTROL_REGEX = re.compile(r'\x1B\[[0-9;]*[A-DJK]')
 
+class MultiLineAnsiHandler:
+    """
+    Configurable handler for any tool with a multi-line display. It can be configured to
+    handle UIs with stable headers and footers, redrawing only the middle section.
+    """
+    def __init__(self, header_lines_count: int = 2, footer_lines_count: int = 2, **kwargs):
+        self.buffer = b""
+        self.is_interactive = sys.stdout.isatty()
+
+        # Set the number of stable header and footer lines.
+        self.header_lines_count = header_lines_count
+        self.footer_lines_count = footer_lines_count
+
+        # When the header is written, this flag is set to true and we don't write the header anymore
+        self.header_captured = False
+        # How many lines the UI block was the last time we drew it, so we know how many lines to erase
+        self.last_drawn_height = 0
+        self.final_log_message = ""
+
+    def __call__(self, byte_chunk: bytes):
+
+        if not self.is_interactive:
+            # If we're in an interactive environment, strip all of the ansi and log the updates at the debug level.
+            clean_text = FULL_ANSI_REGEX.sub('', byte_chunk.decode(sys.stdout.encoding, 'replace')).strip()
+            if clean_text:
+                logger.debug(clean_text)
+            return # No interactive display needed
+
+        # Add the new data to the buffer
+        self.buffer += byte_chunk
+
+        # Only proceed if the buffer contains a newline, signalling the end of a multi-line update from the tool
+        if b'\n' in self.buffer:
+            # Get the last complete update with rsplit. data contains the full update and self.buffer is left with anything else
+            # to be used in the next chunk.
+            data, self.buffer = self.buffer.rsplit(b'\n', 1)
+            # Pass the complete, stable update to main processing engine.
+            self._process_update(data)
+
+    def _process_update(self, full_update_bytes):
+        # Prepare the data.
+        # Decode from raw bytes to a python string
+        full_text = full_update_bytes.decode(sys.stdout.encoding, 'replace')
+        # Strip all ANSI to get pure text
+        clean_text = FULL_ANSI_REGEX.sub('', full_text).strip()
+
+        lines = clean_text.splitlines()
+        if not lines:
+            return
+
+        # Decide whether to do the initial draw or a redraw
+        if not self.header_captured:
+            # First time we draw. Slice the list of lines into header, footer and middle
+            header = lines[:self.header_lines_count]
+            footer = lines[-self.footer_lines_count:] if self.footer_lines_count > 0 else []
+            middle = lines[self.header_lines_count : len(lines) - self.footer_lines_count]
+
+            # Print everything to the terminal the first time.
+            if header:
+                sys.stdout.write('\n'.join(header) + '\n')
+            if middle:
+                sys.stdout.write('\n'.join(middle) + '\n')
+            if footer:
+                sys.stdout.write('\n'.join(footer) + '\n')
+            # Flag that we've already done this.
+            self.header_captured = True
+        else:
+            # Only care about dynamic middle and footer sections
+            middle = lines[self.header_lines_count : len(lines) - self.footer_lines_count]
+            # Calculatehow far up the cursor needs to go to get to the start of the previous dynamic block
+            prev_dynamic_height = self.last_drawn_height - self.header_lines_count - self.footer_lines_count
+            lines_to_move_up = prev_dynamic_height + self.footer_lines_count
+
+            if lines_to_move_up > 0:
+                # ANSI command to move the cursor up N lines.
+                sys.stdout.write(f'\x1b[{lines_to_move_up}A')
+
+            # Erase everything from the cursor's current position to the end of the screen
+            sys.stdout.write('\x1b[J')
+
+            # Draw the new middle and footer to the terminal
+            if middle:
+                sys.stdout.write('\n'.join(middle) + '\n')
+            if self.footer_lines_count > 0:
+                footer = lines[-self.footer_lines_count:]
+                sys.stdout.write('\n'.join(footer) + '\n')
+
+        sys.stdout.flush()
+        self.last_drawn_height = len(lines)
+        self.final_log_message = clean_text
+
+    def flush(self):
+        if self.buffer:
+            self._process_update(self.buffer)
+
+        if self.is_interactive and self.last_drawn_height > 0:
+            sys.stdout.write(f'\x1b[{self.last_drawn_height}A')
+            sys.stdout.write('\x1b[J')
+            sys.stdout.flush()
+
+        if self.final_log_message:
+            logger.info(f"Final status: \n{self.final_log_message}")
+
 class SelectiveAnsiInteractiveHandler:
     def __init__(self, *args, **kwargs):
         self.buffer = b""
